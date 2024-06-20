530580ac25c70eb5194f1abf902bc5762001f526
safuvan <contactmesafwan@gmail.com>
2024-06-20 21:59:39 +0530

    test

--- Python_Problems/test.py
+++ Python_Problems/test.py
@@ -1,2 +1,23 @@
-def sample():
-    pass
+class Vechile:
+    def __init__(self, wheel):
+        self.wheel = wheel
+
+
+class Car(Vechile):
+    def __init__(self, brand, wheel):
+        super().__init__(wheel)
+        self.brand = brand
+    
+    def car_brand(self):
+        print(self.brand)
+    
+    def wheel_count(self):
+        print(self.wheel)
+
+car1 = Car("Benz", 4)
+car1.car_brand()
+car1.wheel_count()
+
+car2 = Car("BMW", 2)
+car2.car_brand()
+car2.wheel_count()
