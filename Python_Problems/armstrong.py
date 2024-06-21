880f33df8c2800545f4ccdc26229f3966db871cb
safuvan <contactmesafwan@gmail.com>
2024-06-21 23:01:08 +0530

    python problem : armstrong

---
+++ Python_Problems/armstrong.py
@@ -0,0 +1,29 @@
+# Check number is Armstrong or not
+
+"""
+The Logic of Armstrong is...
+
+Get the number.
+Take the number digits count.
+sum all the digits raised with power of digits count.
+and check if the provided number and sum is equal, if then it is Armstrong Number.
+    eg:- number = 153
+         result = 1*1*1 + 5*5*5 + 3*3*3   # digits raised to its power which here the digits count is 3.
+
+"""
+
+
+def is_armstrong(num):
+    num_str = str(num)
+    digits_count = len(num_str)
+    result = 0
+
+    for digit in num_str:
+        result += int(digit) ** digits_count
+
+    if num == result:
+        return f"{num} is armstrong"
+    return f"{num} is not armstrong"
+        
+
+print(is_armstrong(1634))
