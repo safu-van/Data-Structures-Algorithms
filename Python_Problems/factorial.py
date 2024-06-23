fedee1e197e3d0912180410d68582a10da81daa7
safuvan <contactmesafwan@gmail.com>
2024-06-23 18:41:02 +0530

    python problem : factorial

---
+++ Python_Problems/factorial.py
@@ -0,0 +1,21 @@
+# Get factorial
+
+"""
+The Logic of Factorial is...
+
+Get the number.
+Multiply from 1 to the number given.
+the result is the factorial of the number.
+    eg:- number = 5
+         result = 1*2*3*4*5     
+
+"""
+
+
+def factorial(num):
+    result = 1
+    for num in range(1, num+1):
+        result *= num
+    return result
+
+print(factorial(5))
