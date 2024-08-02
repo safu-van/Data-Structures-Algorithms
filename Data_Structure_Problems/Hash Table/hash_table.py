45b9fc8c27f3c270a17148f863feebdeacb3d930
safuvan <contactmesafwan@gmail.com>
2024-08-02 10:05:01 +0530

    Hash Table: hash_table.py edited

--- Data_Structure_Problems/Hash Table/hash_table.py
+++ Data_Structure_Problems/Hash Table/hash_table.py
@@ -4,7 +4,6 @@ class HashTable:
     def __init__(self, size):
         self.size = size
         self.array = [None for _ in range(self.size)]
-
     
     # hash function.
     """
@@ -18,19 +17,16 @@ class HashTable:
             hash += ord(char)   # ord() method is used to find ascii value.
         return hash % self.size
     
-
     # to add key, value to hash table.
     def add(self, key, value):
         hash = self.get_hash(key)
         self.array[hash] = value
     
-
     # to get value using key in hash table.
     def get(self, key):
         hash = self.get_hash(key)
         print(self.array[hash])
     
-
     # to delete.
     def delete(self, key):
         hash = self.get_hash(key)
